280ba5b51db94f53276d47d3d9a8b36fa5272c89
Pessa Eemeli <paapessa@eduouka.fi>
2023-11-01 10:40:07 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-BIN_DIR = bin
+BIN_DIR="bin"
 
-GCC_FLAGS = -m32 -ffreestanding
-LD_FALGS = -Ttext 0x1000 $BIN_DIR/entry.bin $BIN_DIR/kernel.o $BIN_DIR/IDT.bin
+GCC_FLAGS="-m32 -ffreestanding"
+LD_FALGS="-Ttext 0x1000 $BIN_DIR/entry.bin $BIN_DIR/kernel.o $BIN_DIR/IDT.bin"
 
-SYSTEM_ARCH = i386
-QEMU_FLAGS = -drive format=raw,file=$BIN_DIR/kernel.img
+SYSTEM_ARCH="i386"
+QEMU_FLAGS="-drive format=raw,file=$BIN_DIR/kernel.img"
 
 nasm boot.s -f bin -o $BIN_DIR/bootsect.bin
 nasm kernel-entry.s -f elf -o $BIN_DIR/entry.bin
